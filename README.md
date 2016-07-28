4549284ff551418d9baecbb468b9e0f8b834d3f9
rafa.hidalgo <rahife@users.noreply.github.com>
2016-07-28 23:42:14 +0700

    Add README file

---
+++ README.md
@@ -0,0 +1,12 @@
+#FAKE CSV DATA GENERATOR
+
+##FILES GENERATED
+
+- Master-area-of-cover.csv
+- Sub-zones-of-cover.csv
+- Country-of-residence-plan-availability.csv
+- Company-premiums-and-benefits.csv
+
+##CSV VALIDATION
+
+http://csvlint.io
--- generate_master_area_of_cover.py
+++ generate_master_area_of_cover.py
@@ -1,9 +1,10 @@
 from faker import Factory
+
 fake = Factory.create()
 
 file = open('Master-area-of-cover.csv', 'w+')
 
-text = 'Worldwide,Worldwide excluding USA,South East Asia,Europe\n'
+text = 'Worldwide,Worldwide excluding USA,South East Asia,Europe\r\n'
 
 print file.read()
 
@@ -11,7 +12,7 @@ def decor(field):
     return ('"' + field + '"') if "," in field else field
 
 for _ in range(0,10):
-    next_row = decor("picha, ole") + ',' + decor(fake.country()) + ',' + decor(fake.country()) + ',' + decor(fake.country()) + ',' + decor(fake.country()) + '\n'
+    next_row = decor(fake.country()) + ',' + decor(fake.country()) + ',' + decor(fake.country()) + ',' + decor(fake.country()) + ',' + decor(fake.country()) + '\r\n'
     text = text + next_row
 
 file.write(text)
