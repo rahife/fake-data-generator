e113c13d9189164db9223c287ac9efffefd93ff3
rafa.hidalgo <rahife@users.noreply.github.com>
2016-07-28 20:45:08 +0700

    First working version

--- generate_master_area_of_cover.py
+++ generate_master_area_of_cover.py
@@ -3,10 +3,15 @@ fake = Factory.create()
 
 file = open('Master-area-of-cover.csv', 'w+')
 
-text = "Worldwide,Worldwide excluding USA,South East Asia,Europe"
-
+text = 'Worldwide,Worldwide excluding USA,South East Asia,Europe\n'
 
 print file.read()
 
+def decor(field):
+    return ('"' + field + '"') if "," in field else field
+
 for _ in range(0,10):
-    print fake.country() + ',' + fake.country() + ',' + fake.country() + ',' + fake.country()
+    next_row = decor("picha, ole") + ',' + decor(fake.country()) + ',' + decor(fake.country()) + ',' + decor(fake.country()) + ',' + decor(fake.country()) + '\n'
+    text = text + next_row
+
+file.write(text)
